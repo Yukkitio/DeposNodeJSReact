0b8815144baf1d75d260bc6b9dc6f104ab517ed4
Victor <Victor20011204@gmail.com>
2023-08-11 18:30:45 +0200

    Optimisation et traduction en FR

--- client/src/components/demo.js
+++ client/src/components/demo.js
@@ -3,53 +3,145 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
-import { Box } from '@mui/material';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Checkbox from '@mui/material/Checkbox';
+import Stack from '@mui/material/Stack';
 
 export default function Demo({ socket, userId }) {
-  const [value, setValue] = useState(30);
+  // État local pour différents composants
+  const [valeurSlider, setValeurSlider] = useState(30);
   const [message, setMessage] = useState('');
+  const [valeurToggleButton, setValeurToggleButton] = useState('bouton1');
+  const [valeurChampTexte, setValeurChampTexte] = useState('');
+  const [valeurSwitch, setValeurSwitch] = useState(false);
+  const [valeurSelect, setValeurSelect] = useState('option1');
+  const [valeurCheckbox, setValeurCheckbox] = useState(false);
 
+  // Gestionnaire d'événement pour le bouton de bascule (ToggleButton)
+  const gererChangementToggleButton = (event, nouvelleValeur) => {
+    setValeurToggleButton(nouvelleValeur);
+    socket.emit('toggleButton', nouvelleValeur);
+  };
+
+  // Gestionnaire d'événement pour le champ de texte (TextField)
+  const gererChangementChampTexte = (event) => {
+    setValeurChampTexte(event.target.value);
+    socket.emit('text', event.target.value);
+  };
+
+  // Gestionnaire d'événement pour l'interrupteur (Switch)
+  const gererChangementSwitch = () => {
+    setValeurSwitch(!valeurSwitch);
+    socket.emit('switch', !valeurSwitch);
+  };
+
+  // Gestionnaire d'événement pour la liste déroulante (Select)
+  const gererChangementSelect = (event) => {
+    setValeurSelect(event.target.value);
+    socket.emit('select', event.target.value);
+  };
+
+  // Gestionnaire d'événement pour la case à cocher (Checkbox)
+  const gererChangementCheckbox = (event) => {
+    setValeurCheckbox(event.target.checked);
+    socket.emit('checkbox', event.target.checked);
+  };
+
+  // Gestionnaire d'événement pour le bouton "Parler au serveur"
+  const gererCliqueButton = () => {
+    console.log(`Message envoyé au serveur`);
+    socket.emit('message', `Salut ! Je suis un client`);
+  };
+  // Écouteur d'événement pour recevoir les messages du serveur
   socket.on('message', (data) => {
     setMessage(data);
   });
 
-  const handleClick = () => {
-    console.log(`Message envoyé au server`);
-    socket.emit('message', `Message de : ${userId}`);
+  // Gestionnaire d'événement pour le changement du Slider
+  const gererChangementSlider = (event, nouvelleValeur) => {
+    setValeurSlider(nouvelleValeur);
   };
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  const handleSliderMouseUp = () => {
-    console.log('Slider value on mouse up:', value);
-    socket.emit('slider', value);
+  // Gestionnaire d'événement pour le relâchement de la souris après avoir utilisé le Slider
+  const gererRelachementSlider = () => {
+    console.log('Valeur du Slider au relâchement de la souris :', valeurSlider);
+    socket.emit('slider', valeurSlider);
   };
 
   return (
-    <Paper elevation={8} sx={{width:'40%', p:'2vh'}}>
+    <Paper elevation={8} sx={{ width: '40%', p: '2vh' }}>
       <Typography>Showcase</Typography>
-      <Typography>User ID: {userId}</Typography>
-      <Button variant="contained" color="primary" onClick={handleClick} style={{ marginTop: '16px' }}>
-        Send Message
-      </Button>
-      <p>{message}</p>
-      <Box
-        style={{
-          width: '80%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginTop: '16px',
-          marginLeft: 'auto',
-          marginRight: 'auto' // Ajout de marges automatiques pour centrer horizontalement
-        }}
-      >
-        <Slider aria-label="Volume" value={value} onChange={handleChange} onMouseUp={handleSliderMouseUp} />
-        <Typography>Value:</Typography>
-        <Typography>{value}</Typography>
-      </Box>
+      <Typography>ID de l'utilisateur : {userId}</Typography>
 
+      {/* Toggle Button */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <ToggleButtonGroup
+          value={valeurToggleButton}
+          exclusive
+          onChange={gererChangementToggleButton}
+        >
+          <ToggleButton value="button1">Bouton 1</ToggleButton>
+          <ToggleButton value="button2">Bouton 2</ToggleButton>
+          <ToggleButton value="button3">Bouton 3</ToggleButton>
+        </ToggleButtonGroup>
+      </Paper>
+
+      {/* Text Field */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <TextField
+          label="Text Field"
+          value={valeurChampTexte}
+          onChange={gererChangementChampTexte}
+        />
+      </Paper>
+
+      {/* Switch */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <Typography>Switch: {valeurSwitch ? 'On' : 'Off'}</Typography>
+        <Switch checked={valeurSwitch} onChange={gererChangementSwitch} />
+      </Paper>
+
+      {/* Select */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <Select
+          value={valeurSelect}
+          onChange={gererChangementSelect}
+          label="Select"
+        >
+          <MenuItem value="option1">Option 1</MenuItem>
+          <MenuItem value="option2">Option 2</MenuItem>
+          <MenuItem value="option3">Option 3</MenuItem>
+        </Select>
+      </Paper>
+
+      {/* Checkbox */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <Typography>Checkbox: {valeurCheckbox ? 'Checked' : 'Unchecked'}</Typography>
+        <Checkbox
+          checked={valeurCheckbox}
+          onChange={gererChangementCheckbox}
+        />
+      </Paper>
+
+      {/* Slider */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
+          <Slider aria-label="Volume" value={valeurSlider} onChange={gererChangementSlider} onMouseUp={gererRelachementSlider} />
+          <Typography>Valeur : {valeurSlider}</Typography>
+        </Stack>
+      </Paper>
+
+      {/* Button */}
+      <Paper elevation={4} sx={{ m: '2vh', p: '2vh' }}>
+        <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
+          <Button variant="contained" color="primary" onClick={gererCliqueButton}>Parler au serveur</Button>
+          <Typography>{message}</Typography>
+        </Stack>
+      </Paper>
     </Paper>
   );
 }
--- server.js
+++ server.js
@@ -3,50 +3,70 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
+// Initialisation de l'application Express
 const app = express();
+
+// Création du serveur HTTP en utilisant l'application Express
 const server = http.createServer(app);
+
+// Configuration de Socket.IO avec les options CORS
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: 'http://localhost:3000', // Remplacez par l'URL de votre frontend
     methods: ['GET', 'POST'],
     credentials: true,
   },
 });
 
+// Écouteur d'événement pour gérer les connexions des utilisateurs
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('Un utilisateur connecté');
 
-  // Generate a unique ID for the user
+  // Générer un ID unique pour l'utilisateur
   const userId = socket.id;
 
-  // Send the user's ID to the connected user
+  // Envoyer l'ID de l'utilisateur connecté à celui-ci
   socket.emit('userId', userId);
 
+  // Écouteurs d'événements pour différents types d'interactions
+
   socket.on('message', (data) => {
-    console.log(`Received from user ${userId}:`, data);
-    socket.emit('message', `message reçu (serveur)`);
+    console.log(`Reçu de l'utilisateur ${userId}:`, data);
+    socket.emit('message', `Salut de retour ! (serveur)`);
   });
 
-
-  socket.on('slider', (data) => {
-    console.log(`user ${userId} set value to :`, data);
+  socket.on('toggleButton', (data) => {
+    console.log(`Utilisateur ${userId} a réglé le bouton sur :`, data);
   });
 
-  socket.on('radio', (data) => {
-    console.log(`user ${userId} change Radio to :`, data);
+  socket.on('text', (data) => {
+    console.log(`Utilisateur ${userId} a modifié le texte en :`, data);
+  });
+
+  socket.on('switch', (data) => {
+    console.log(`Utilisateur ${userId} a changé l'état du switch en :`, data);
   });
 
   socket.on('select', (data) => {
-    console.log(`user ${userId} select :`, data);
+    console.log(`Utilisateur ${userId} a sélectionné :`, data);
   });
 
+  socket.on('checkbox', (data) => {
+    console.log(`Utilisateur ${userId} a coché :`, data);
+  });
 
+  socket.on('slider', (data) => {
+    console.log(`Utilisateur ${userId} a déplacé le curseur vers :`, data);
+  });
+
+  // Écouteur d'événement pour gérer la déconnexion de l'utilisateur
   socket.on('disconnect', () => {
-    console.log(`User ${userId} disconnected`);
+    console.log(`Utilisateur ${userId} déconnecté`);
   });
 });
 
+// Définition du port d'écoute du serveur
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Le serveur fonctionne sur le port ${PORT}`);
 });
